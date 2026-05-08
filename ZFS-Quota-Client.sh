8f118013680c13c80cffd5c9e9a9f7f7576f2224
Adam Boutcher <adam.j.boutcher@durham.ac.uk>
2026-05-08 16:46:40 +0100

    Move main code to function. Better variable quotation and surrounding. Handle argument slighly cleaner.

--- ZFS-Quota-Client.sh
+++ ZFS-Quota-Client.sh
@@ -11,16 +11,45 @@ servers[1]="/mnt/storage"
 grpsrvs[0]="/mnt/groups"
 
 ################################################################################
-if [ -z "$1" ]; then
-  QUSER=$USER
-else
-  QUSER=$1
-fi
+QUSER=${1:-$USER}
 
 function check_bin() {
-  if ! command -v "$1" &>/dev/null; then
+  if ! command -v "${1}" &>/dev/null; then
     echo "$1 cannot be found. Please install it or add it to the path. Exiting."
     exit 1
+  fi
+}
+
+# This loops the quota checking in a function.
+# Usage: quota_check (int)$id (string)$quota_dir (string)[$grp_name]
+function zquota_check() {
+  if [[ -n "${1}" && -n "${2}" ]]; then
+
+    id="${1}"
+    server="${2}"
+
+    # This is a better matching than the previous,
+    #  it should now look for a start of line and also '::'.
+    zquota=$(grep "^${id}::" "${server}/quota.zfs" 2>/dev/null);
+    if [[ -n "${zquota}" ]]; then
+      zused=$(echo "${zquota}" | awk -F'::' '{print $2}' | numfmt --to=iec);
+      if [[ $zused == "nan" ]]; then
+        zused=0;
+      fi
+      ztotal=$(echo "${zquota}" | awk -F'::' '{print $3}');
+      if [[ $ztotal != "none" ]]; then
+        ztotal=$(echo "${ztotal}" | numfmt --to=iec);
+      fi
+      zperc=$(echo "${zquota}" | awk -F'::' '{print $4}');
+      zage=$(date +"%c" -d @$(stat -c %Z ${server}/quota.zfs))
+      # Display a nice goup name.
+      if [[ -z "${3}" ]]; then
+        mount_name="${server}"
+      else
+        mount_name="${server} (${3})"
+      fi
+      printf ' %-35s %-15s %-10s %-20s\n' "${mount_name}" "${zused} (${zperc})" "${ztotal}" "${zage}";
+    fi
   fi
 }
 
@@ -32,59 +61,21 @@ check_bin id
 check_bin date
 check_bin getent
 
-printf "\n Storage Quota v2.6 BETA - This *might* sometimes break."
+printf "\n Storage Quota v2.6."
 printf "\n Group reports are entire group usage not individual.\n"
-printf "\n Quota Report for $QUSER\n"
+printf "\n Quota Report for ${QUSER}\n"
 printf ' %-35s %-15s %-10s %-20s\n' "Mount Point" "Used" "Total" "Last Checked";
 
 # User Checks
-QUID=$(id -u $QUSER 2>/dev/null)
-for i in "${servers[@]}"; do
-  #zquota=$(grep -F "^$QUID::" $i/quota.zfs 2>/dev/null);
-  zquota=$(cat $i/quota.zfs 2>/dev/null | grep $QUID 2>/dev/null);
-  if [[ ! -z "$zquota" ]]; then
-    for ii in $zquota; do
-      zuid=$(echo $ii | awk -F'::' '{print $1}')
-      if  [[ $zuid == $QUID ]]; then
-        zused=$(echo $ii | awk -F'::' '{print $2}' | numfmt --to=iec);
-        if [[ $zused == "nan" ]]; then
-          zused=0;
-        fi
-        ztotal=$(echo $ii | awk -F'::' '{print $3}');
-        if [[ $ztotal != "none" ]]; then
-          ztotal=$(echo $ztotal | numfmt --to=iec);
-        fi
-        zperc=$(echo $ii | awk -F'::' '{print $4}');
-        zage=$(date +"%c" -d @$(stat -c %Z $i/quota.zfs))
-        printf ' %-35s %-15s %-10s %-20s\n' "$i" "$zused ($zperc)" "$ztotal" "$zage";
-      fi
-    done
-  fi
+QUID=$(id -u "${QUSER}" 2>/dev/null)
+for server in "${servers[@]}"; do
+  zquota_check "${QUID}" "${server}"
 done;
 
 # Group Checks
-for grp in $(id -G $QUSER 2>/dev/null); do
-  for i in "${grpsrvs[@]}"; do
-    zquota=$(cat $i/quota.zfs 2>/dev/null | grep $grp 2>/dev/null);
-    if [[ ! -z "$zquota" ]]; then
-      for ii in $zquota; do
-        zgid=$(echo $ii | awk -F'::' '{print $1}')
-        if  [[ $zgid == $grp ]]; then
-          zused=$(echo $ii | awk -F'::' '{print $2}' | numfmt --to=iec);
-          if [[ $zused == "nan" ]]; then
-            zused=0;
-          fi
-          ztotal=$(echo $ii | awk -F'::' '{print $3}');
-          if [[ $ztotal != "none" ]]; then
-            ztotal=$(echo $ztotal | numfmt --to=iec);
-          fi
-          zperc=$(echo $ii | awk -F'::' '{print $4}');
-          zage=$(date +"%c" -d @$(stat -c %Z $i/quota.zfs))
-          zgnm=$(getent group $grp | awk -F':' '{print $1}')
-          printf ' %-35s %-15s %-10s %-20s\n' "$i ($zgnm)" "$zused ($zperc)" "$ztotal" "$zage";
-        fi
-      done
-    fi
+for grpid in $(id -G "${QUSER}" 2>/dev/null); do
+  for server in "${grpsrvs[@]}"; do
+    zquota_check "${grpid}" "${server}" "$(getent group $grp | awk -F':' '{print $1}')"
   done;
 done;
 
